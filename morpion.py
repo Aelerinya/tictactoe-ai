be65ea67ab7f3001194edef054a2f7097b447fc1
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-12-12 18:53:37 +0100

    Save every game state to dataset

--- morpion.py
+++ morpion.py
@@ -6,7 +6,6 @@ class morpion_game():
     def __init__(self, player1, player2):
         self.turn = 0
         self.players = [player1, player2]
-        random.shuffle(self.players)
         self.grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
     def is_winner(self, player):
@@ -28,6 +27,65 @@ class morpion_game():
 
         return 0
 
+    def run(self):
+        while self.turn < 9:
+            previous_state = self.flatten_grid()
+
+            player = self.players[self.turn % 2]
+            reward = 0
+            #Player do a move
+            move = player.play(self.flatten_grid(), player.icon)
+            if self.grid[move[0]][move[1]] == 0:
+                self.grid[move[0]][move[1]] = player.icon
+            else:
+                reward = -1
+            #Add new state to player dataset
+            state = self.flatten_grid()
+            player.add_data((previous_state, state, move, reward))
+            #Does the player win ?
+            if self.is_winner(player.icon):
+                return player.name
+            self.turn += 1
+        return "Tie"
+
     def log(self):
         for i in self.grid:
             print(i)
+
+    def flatten_grid(self):
+        return tuple([item for sublist in self.grid for item in sublist])
+
+class morpion_player():
+    def __init__(self, name, icon):
+        self.dataset = []
+        self.states = {}
+        self.name = name
+        self.icon = icon
+
+    def play(self, state, player):
+        for i in [0, 3, 6]:
+            print(state[i : i + 3])
+        line = int(input("line:"))
+        column = int(input("column:"))
+        return (line, column)
+
+    def add_data(self, move):
+        self.dataset.append(move)
+
+def main():
+    p1 = morpion_player("IA", "X")
+    p2 = morpion_player("IA 2", "O")
+    game = morpion_game(p1, p2)
+    winner = game.run()
+    if p1.name == winner:
+        p1.add_data((game.flatten_grid(), 10))
+        p2.add_data((game.flatten_grid(), -10))
+    elif p2.name == winner:
+        p1.add_data((game.flatten_grid(), -10))
+        p2.add_data((game.flatten_grid(), 10))
+    print(f"Winner : {winner}")
+    print(p1.dataset)
+    print(p2.dataset)
+
+if __name__ == "__main__":
+    main()
